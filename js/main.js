24c5f6ab02bc46609408c44308b204cb9fac1cc9
Tarikk_ <117632275+KomtGoedd@users.noreply.github.com>
2023-12-21 12:10:39 +0100

    3.0

--- js/main.js
+++ js/main.js
@@ -62,3 +62,44 @@ function padZero(number) {
 updateClock();
 
   
+
+
+async function prayerTimesAms() {
+    try {
+      const response = await fetch("https://api.aladhan.com/v1/calendar/2023/12?latitude=52.370216&longitude=4.895168&method=2");
+      const times = await response.json();
+      console.log(times.data[0]);
+  
+    //   console.log('Fajr time:', times.data[0].timings.Fajr);
+  
+    
+      const prayerTimesContainer2 = document.getElementById('prayer-times-container-2');
+      prayerTimesContainer2.innerHTML = ''; 
+  
+
+      console.log(times.data[0]);
+    //   times.data[0].forEach(day => {
+        const date = times.data[0].date.readable;
+        const fajr = times.data[0].timings.Fajr;
+        const dhuhr = times.data[0].timings.Dhuhr;
+        const asr = times.data[0].timings.Asr;
+        const maghrib = times.data[0].timings.Maghrib;
+        const isha = times.data[0].timings.Isha;
+  
+    
+        prayerTimesContainer2.innerHTML += `
+          <div>
+            <p>Fajr: ${fajr}</p>
+            <p>Dhuhr: ${dhuhr}</p>
+            <p>Asr: ${asr}</p>
+            <p>Maghrib: ${maghrib}</p>
+            <p>Isha: ${isha}</p>
+          </div>
+        `;
+    //   });
+    } catch (error) {
+      console.error('Er is een fout opgetreden bij het ophalen van de gegevens:', error);
+    }
+  }
+  
+  prayerTimesAms();
